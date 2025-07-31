0bb59967c0bad3b4a95282013a04998b2cf6dc92
jmanuel <manuel.gomez.quispe@gmail.com>
2025-07-31 18:34:39 -0500

    feat:lista tachado closure

--- index.js
+++ index.js
@@ -1,40 +1,39 @@
-class List {
-  constructor(listId, titulo, listArray) {
-    const div = document.createElement("div");
-    div.classList.add(listId);
-    const ul = document.createElement("ul");
-    const h2 = document.createElement("h2");
-    document.body.append(div);
-    div.append(h2);
-    h2.textContent = titulo;
-    div.append(ul);
+function createList(listId, titulo, listArray) {
+  const div = document.createElement("div");
+  div.classList.add(listId);
+  const ul = document.createElement("ul");
+  const h2 = document.createElement("h2");
+  document.body.append(div);
+  div.append(h2);
+  h2.textContent = titulo;
+  div.append(ul);
 
-    const candidatos = listArray;
+  const candidatos = listArray;
+  candidatos.forEach((candidato) => {
+    const li = document.createElement("li");
+    li.textContent = candidato;
+    ul.append(li);
+  });
 
-    candidatos.forEach((candidato) => {
-      const li = document.createElement("li");
-      li.textContent = candidato;
-      ul.append(li);
-    });
-
-    ul.addEventListener("click", function (event) {
-      if (event.target.tagName === "LI") {
-        event.target.classList.toggle("selected");
-      }
-    });
-  }
+  ul.addEventListener("click", function (event) {
+    if (event.target.tagName === "LI") {
+      event.target.classList.toggle("selected");
+    }
+  });
 }
 
-const list2 = new List("listId", "Cual es tu postre favorito?", [
-  "Pie de limon",
-  "3 leches",
-  "leche asada",
-  "torta de chocolate",
-]);
-const list1 = new List("listId", "quien ganara las elecciones?", [
+const list1 = createList("listId", "quien ganara las elecciones?", [
   "Porky",
   "Vizcarra",
   "Antauro",
   "Keiko",
   "Butters",
 ]);
+const list2 = createList("listId", "Cual es tu postre favorito?", [
+  "Pie de limon",
+  "3 leches",
+  "leche asada",
+  "torta de chocolate",
+]);
+
+console.log(list1);
